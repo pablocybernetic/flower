cb3e29640dae2956523971dbd00a7b892b662f70
pablocybernetic <gitaup08@gmail.com>
2024-01-18 15:33:31 +0300

    .

--- resources/views/place_order.blade.php
+++ resources/views/place_order.blade.php
@@ -12,6 +12,7 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
           
+          
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
