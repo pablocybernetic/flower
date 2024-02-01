e154405d3f4d0140d8a6d49580f8249c0d3c9b33
pablocybernetic <gitaup08@gmail.com>
2024-02-01 16:32:18 +0300

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
 
             
             <div class="menu-item-carousel">
-                <div class="col-lg-12">
+                <div class="col-lg-6">
                     <style>.grid-container {
                         display: grid;
                         grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); /* Adjust the column width as needed */
@@ -76,9 +76,9 @@
 
                         
                             
-<!-- *****  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img class="rounded-t-lg" src="https://www.kindpng.com/picc/m/139-1392125_oak-tree-white-background-hd-png-download.png" alt="" />
+        <img class="rounded-t-lg" src="https://www.kindpng.com/picc/m/139-1392125_oak-tree-white-background-hd-png-download.png" alt=""  style="height: 300px;"/>
     </a>
     <div class="p-5">
         <a href="#">
@@ -89,20 +89,20 @@
             Read more
              {{-- <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"> --}}
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
+            {{-- </svg> --}}
         </a>
     </div>
 </div>
- ***** -->
+ 
                       
                         @foreach($menu as $product)
                        
-                        <div class="item pt-1 pb-3 pl-0 pr-5 ">
+                        <div class="grid-item  ">
     
                         <?php
                             $img=$product->image;
                         ?>
-                            <div class='card' style="background-image: url({{asset('assets/images/'.$img)}})"> 
+                            <div class='card' style="background-image: url({{asset('assets/images/'.$img)}}); background-size: 300px; background-position: center; height: 300px ;"> 
     
                                 <div class="price"><h6>Ksh{{ $product->price }}</h6>
                                 @if($product->available!="Stock")
