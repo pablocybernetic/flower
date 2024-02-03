86f6bf721450cd51e251738e71c16711f1cdeb10
pablocybernetic <gitaup08@gmail.com>
2024-02-03 11:53:25 +0300

    .

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -221,8 +221,8 @@
           
           </ul>
           <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <input class="form-control mr-sm-2" style="border-radius: 20px;" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-primary my-2 my-sm-0"style="border-radius: 20px;" type="submit">Search</button>
           </form>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
