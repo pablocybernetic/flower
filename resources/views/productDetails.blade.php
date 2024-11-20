89f5ef8b56d770fd1ee6bb79c27194aeb5f0bfba
pablocybernetic <gitaup08@gmail.com>
2024-11-20 21:28:25 +0300

    fixes

--- resources/views/productDetails.blade.php
+++ resources/views/productDetails.blade.php
@@ -7,25 +7,25 @@
     <div class="container">
       <div class="row gx-5">
         <aside class="col-lg-6">
-          <div class="border rounded-4 mb-3 d-flex justify-content-center">
+          <div class="mb-3 border rounded-4 d-flex justify-content-center">
             <a data-fslightbox="mygalley" class="rounded-4" target="_blank" data-type="image" href="{{asset('assets/images/'.$product->image)}}">
               <img style="max-width: 100%; max-height: 100vh; margin: auto;" class="rounded-4 fit" src="{{asset('assets/images/'.$product->image)}}" />
             </a>
           </div>
-          <div class="d-flex justify-content-center mb-3">
-            <a data-fslightbox="mygalley" class="border mx-1 rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
+          <div class="mb-3 d-flex justify-content-center">
+            <a data-fslightbox="mygalley" class="mx-1 border rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
               <img width="60" height="60" class="rounded-2" src="{{asset('assets/images/'.$product->image)}}" />
             </a>
-            <a data-fslightbox="mygalley" class="border mx-1 rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
+            <a data-fslightbox="mygalley" class="mx-1 border rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
               <img width="60" height="60" class="rounded-2" src="#" />
             </a>
-            <a data-fslightbox="mygalley" class="border mx-1 rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
+            <a data-fslightbox="mygalley" class="mx-1 border rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
               <img width="60" height="60" class="rounded-2" src="#" />
             </a>
-            <a data-fslightbox="mygalley" class="border mx-1 rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
+            <a data-fslightbox="mygalley" class="mx-1 border rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
               <img width="60" height="60" class="rounded-2" src="#" />
             </a>
-            <a data-fslightbox="mygalley" class="border mx-1 rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
+            <a data-fslightbox="mygalley" class="mx-1 border rounded-2" target="_blank" data-type="image" href="#" class="item-thumb">
               <img width="60" height="60" class="rounded-2" src="#" />
             </a>
           </div>
@@ -34,11 +34,11 @@
         </aside>
         <main class="col-lg-6">
           <div class="ps-lg-3">
-            <h4 class="title text-dark">
+            <h4 class="titl text-dark" style="text-align: left">
               {{ $product->name }}
           </h4>
-            <div class="d-flex flex-row my-3">
-              {{-- <div class="text-warning mb-1 me-2">
+            <div class="flex-row my-3 d-flex">
+              {{-- <div class="mb-1 text-warning me-2">
                 <i class="fa fa-star"></i>
                 <i class="fa fa-star"></i>
                 <i class="fa fa-star"></i>
@@ -49,8 +49,8 @@
                   
                 </span>
               </div> --}}
-              <span class="text-muted"><i class="fas fa-shopping-basket fa-sm mx-1"></i>154 orders</span>
-              <br><br>
+              {{-- <span class="text-muted"><i class="mx-1 fas fa-shopping-basket fa-sm"></i>154 orders</span> --}}
+              <br>
             </div>
             <span class="text-success ms-2">In stock</span>
 
@@ -80,10 +80,10 @@
   
             <hr />
   
-            <div class="row mb-4">
+            <div class="mb-4 row">
               {{-- <div class="col-md-4 col-6">
                 <label class="mb-2">Size</label>
-                <select class="form-select border border-secondary" style="height: 35px;">
+                <select class="border form-select border-secondary" style="height: 35px;">
                   <option>Small</option>
                   <option>Medium</option>
                   <option>Large</option>
@@ -95,13 +95,13 @@
             <form method="post" action="{{ route('cart.store', $product->id) }}">
                 @csrf
                 <label class="mb-2 d-block">Quantity</label>
-                <div class="row row-md-4 row-6 mb-3">
-                    <div class="input-group mb-3" style="width: 170px;">
-                        <button class="btn btn-white border border-secondary px-3" type="button" id="decrementButton" data-mdb-ripple-color="dark">
+                <div class="mb-3 row row-md-4 row-6">
+                    <div class="mb-3 input-group" style="width: 170px;">
+                        <button class="px-3 border btn btn-white border-secondary" type="button" id="decrementButton" data-mdb-ripple-color="dark">
                             <i class="fa fa-minus"></i>
                         </button>
-                        <input type="text" class="form-control text-center border border-secondary" placeholder="1" id="quantityInput" name="number"  value="1" aria-label="Quantity" aria-describedby="decrementButton incrementButton" />
-                        <button class="btn btn-white border border-secondary px-3" type="button" id="incrementButton" data-mdb-ripple-color="dark">
+                        <input type="text" class="text-center border form-control border-secondary" placeholder="1" id="quantityInput" name="number"  value="1" aria-label="Quantity" aria-describedby="decrementButton incrementButton" />
+                        <button class="px-3 border btn btn-white border-secondary" type="button" id="incrementButton" data-mdb-ripple-color="dark">
                             <i class="fa fa-plus"></i>
                         </button>
                     </div>
@@ -115,9 +115,7 @@
                 
                
             </form>
-            <a href="/cart" class="btn btn-warning shadow-0"> Buy now </a>
             
-            <a href="#" class="btn btn-light border border-secondary py-2 icon-hover px-3"> <i class="me-1 fa fa-heart fa-lg"></i> Save </a>
           </div>
         </main>
       </div>
@@ -146,17 +144,17 @@
             <div class="menu-item-carousel">
                 <div class="grid-container row gx-4">
                     @foreach($products as $product)
-                    <div class="col-6 col-md-3 col-lg-3 mb-4">
+                    <div class="mb-4 col-6 col-md-3 col-lg-3">
                         <div class="card">
-                            {{-- <i class="fa fa-pagelines fa-lg pt-3 pb-1 px-3"></i> --}}
+                            {{-- <i class="px-3 pt-3 pb-1 fa fa-pagelines fa-lg"></i> --}}
                             <a href="/menu/{{ $product->id }}">
                                 <div style="padding-bottom: 100%; position: relative;">
                                     <img src="{{asset('assets/images/'.$product->image)}}" class="card-img-top img-fluid" alt="Product Image" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;">
                                 </div></a>
                             <div class="card-body">
                                 <h6 class="card-title fs-5">{{ $product->name }}</h6>
-                                {{-- <p class="card-text text-muted mb-3 description">{{ $product->description }}</p> --}}
-                                <div class="d-flex justify-content-between mb-3">
+                                {{-- <p class="mb-3 card-text text-muted description">{{ $product->description }}</p> --}}
+                                <div class="mb-3 d-flex justify-content-between">
                                     <span>Total</span>
                                     <span class="fs-6">Ksh {{ $product->price }}</span>
                                 </div>
