2011c1cef09b61b531bb431f038f61d4f3ddd465
pablocybernetic <gitaup08@gmail.com>
2024-12-21 17:11:38 +0300

    .

--- resources/views/blog_details.blade.php
+++ resources/views/blog_details.blade.php
@@ -3,7 +3,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <!-- Bootstrap JS (for toggle functionality) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 <br><br>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style type="text/css">
